bbc09a1c28e3c9ce85842770f1306efe626d4125
bozdel <bondarevv.slavavv@gmail.com>
2020-11-30 22:34:01 +0700

    my second commit

--- makefile
+++ makefile
@@ -1,6 +1,8 @@
-all: prog.o
-	gcc prog.o -o prog
+all: prog.o func.o
+	gcc prog.o func.o -o sum_hello
 prog.o: prog.c
 	gcc -c prog.c
+func.o: func.c
+	gcc -c fun.c
 clean:
 	rm *.o
--- prog.c
+++ prog.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <func.h>
+
 
 int main() {
 	printf("hello, world!\n");
+	int a = 5;
+	int b = 10;
+	int s = sum(a,b);
+	printf("%d\n", s);
 	return 0;
 }
