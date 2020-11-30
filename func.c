f6d0ab55984c8ada164671c659b429e897f58564
bozdel <bondarevv.slavavv@gmail.com>
2020-11-30 22:45:47 +0700

    third commit

--- func.c
+++ func.c
@@ -1,3 +1,16 @@
 int sum(int a, int b) {
 	return a + b;
 }
+
+int mul(int a, int b) {
+	return a * b;
+}
+
+int poww(int a, int p) {
+	int res = 1;
+	while (p > 0) {
+		res *= a;
+		p--;
+	}
+	return res;
+}
--- func.h
+++ func.h
@@ -3,4 +3,6 @@
 
 int sum(int a, int b);
 
+int mul(int a, int b);
+
 #endif
