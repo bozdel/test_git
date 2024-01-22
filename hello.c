d7bbb063a8cafeb30417a5b6e904726529b03daf
bozdel2 <bozdel@example.me>
2024-01-22 19:52:07 +0700

    add hello print

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void my_print(void) {
+	printf("hello\n");
+}
--- prog.c
+++ prog.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <func.h>
+#include "func.h"
 
+void my_print(void);
 
 int main() {
 	printf("hello, world!\n");
+	my_print();
 	int a = 2;
 	int b = 4;
-	int s = sum(a,pow(a, b));
+	int s = sum(a,poww(a, b));
 	printf("%d\n", s);
 	return 0;
 }
