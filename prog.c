2db43dd5d3fe91184157f4288f8e9dcfff52384a
bozdel2 <bozdel@example.me>
2024-01-22 21:12:39 +0700

    commit from detached

--- prog.c
+++ prog.c
@@ -2,11 +2,21 @@
 #include <func.h>
 
 
+void print_hello(void) {
+	printf("hello\n");
+}
+
+void print_goodbye(void) {
+	printf("goodbye\n");
+}
+
 int main() {
 	printf("hello, world!\n");
+	print_hello();
 	int a = 2;
 	int b = 4;
 	int s = sum(a,pow(a, b));
 	printf("%d\n", s);
+	print_goodbye();
 	return 0;
 }
